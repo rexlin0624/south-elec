24e21efaf26b62be737a9af4242b541fd497dfcb
Rex <rexlin0624@gmail.com>
2019-08-02 22:27:00 +0800

    fix

--- phpcms/modules/product/templates/product_list.tpl.php
+++ phpcms/modules/product/templates/product_list.tpl.php
@@ -26,6 +26,7 @@ include $this->admin_tpl('header', 'admin');
                         <td align="center"><?php echo date('Y-m-d H:i:s', $info['created_at'])?></td>
                         <td align="center">
                             <span style="height:22">
+                                <span style="height:22"><a href="/caches/pdf/<?php echo $info['id']; ?>.pdf" target="_blank">PDF</a></span> |
                                 <span style="height:22"><a href="?m=product&c=product&a=add&id=<?php echo $info['id']?>&menuid=<?php echo $_GET['menuid']?>">修改</a></span> |
                                 <span style="height:22"><a href="?m=product&c=product&a=delete&id=<?php echo $info['id']?>" onclick="return confirm('<?php echo L('confirm', array('message'=>addslashes(new_html_special_chars($info['title']))))?>')">删除</a>
                             </span>
