4cdbab4b4cc8e972e0f2ab0ba2a28db25b180784
Rex <rexlin0624@gmail.com>
2019-10-29 08:48:05 +0800

    fix

--- usb/list.js
+++ usb/list.js
@@ -18,7 +18,7 @@ function getUrlParameter(name) {
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+}
 
 window.onload = function () {
     console.log(getUrlParameter('fid'));
